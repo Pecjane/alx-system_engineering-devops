26366d3170e2987b345c3f774b33e7109167712f
Pecjane <promisebarth@gmail.com>
2024-02-24 14:41:58 -0800

    puppetss

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+#  Installing flask from pip3.
+
+package {'python3-pip':
+  ensure   => 'installed',
+  provider => 'apt'
+}
+
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip']
+}
